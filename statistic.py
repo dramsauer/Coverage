6628fc7c4556f7dee0390bc667f002f90743a5a6
dramsauer <dominik.ramsauer@stud.uni-regensburg.de>
2018-08-06 14:49:59 +0200

    Initial Commit

--- statistic.py
+++ statistic.py
@@ -1,3 +1,9 @@
 from text_coverage_data import wds_universe, sets_universe
 
 # Simulated Annealing
+
+print("wds_universe: ")
+print(wds_universe.__class__)
+
+# for i in wds_universe:
+#    print(i)
