ae60eb467bdc619b6bc410fb86cd6f62d360a946
dramsauer <dominik.ramsauer@stud.uni-regensburg.de>
2018-09-18 17:14:00 +0200

    Implementing the local-search-heuristic

--- src/simulated_annealing.py
+++ src/simulated_annealing.py
@@ -1,4 +1,8 @@
-from src.greedy import greedy_by_balas
+import random
+from collections import defaultdict
+from copy import deepcopy
+
+from src.greedy import greedy_by_balas_with_coverage_matrix
 from src.preprocesses import *
 from text_coverage_data import sets_universe
 
@@ -32,19 +36,22 @@ def simulated_annealing(sets, elements, neighbourhood_scale, search_depth, print
 
     print("Preprocesses.")
     print("Finding a feasable solution via greedy heuristic...")
-    feasable_greedy_solution = greedy_by_balas(sets=sets, elements=elements, print_logs=print_logs)
+    feasable_greedy_solution, amount_elements_covered_dict = greedy_by_balas_with_coverage_matrix(sets=sets, elements=elements, print_logs=print_logs)
+
+    set_collection = deepcopy(sets)
 
 
-
+    # 0.
     print("Initialization.")
     solution_indices = set()
-    not_in_solution_list = list()
-    current_solution_list = get_set_list_of_solution_indices(collection=sets, solution_indices=feasable_greedy_solution)
+    removed_from_solution = list()
 
-    set_lengths = compute_set_lengths(current_solution_list)
+    current_solution_list_indices = list(feasable_greedy_solution)
+
+    set_lengths = compute_set_lengths(get_set_list_of_solution_indices(collection=sets, solution_indices=current_solution_list_indices))
     d = 0
-    D = neighbourhood_scale * len(feasable_greedy_solution)
-    E = search_depth * max(set_lengths)
+    D = neighbourhood_scale * len(current_solution_list_indices)
+    maximum_set_length_allowed = search_depth * max(set_lengths)
 
 
 
@@ -53,10 +60,79 @@ def simulated_annealing(sets, elements, neighbourhood_scale, search_depth, print
     """
     print("\nMain Algorithm.")
     print("Solution optimization by simulated annealing approach")
-    print("\nSearch for better solution sets...")
+    #print("\nSearch for better solution sets:")
 
-    #while d != D:
-    #    current_set = random.sample()
+    print("Removing sets from solution randomly...")
+    while d <= D:
+        # 1. Randomly select a set from solution
+        current_set_index = random.choice(current_solution_list_indices)
+
+        # 2. Move the set from solution to not-covered;
+        removed_from_solution.append(current_set_index)
+        current_solution_list_indices.remove(current_set_index)
+
+        for element in set_collection[current_set_index]:
+            amount_elements_covered_dict[element] -= 1
+
+        d += 1
+
+
+    print("\nCheck which elements now got uncovered...")
+    print("Checking which sets are short enough to be brought into solution again")
+    print("and how many elements get 'recovered' by those sets...\n")
+    print("---Amount Elements uncovered now: ", len(amount_elements_covered_dict), "\n")
+
+    uncovered_elements_existing = True
+    # If all elements are still covered, go to step 6, otherwise continue with step 4.
+    while uncovered_elements_existing: # should be True if that dict has entries!
+        # 3. Update the dict: set the elements that are still covered (-> value is higher than 0)
+        uncovered_count = 0
+        for element in amount_elements_covered_dict:
+            if amount_elements_covered_dict[element] == 0:
+                uncovered_count += 1
+        print("Uncovered Words: ", uncovered_count)
+        if uncovered_count == 0:
+            break
+
+
+
+        # 4. Make a dict of sets which have a length that lower-equals maximum_set_length_allowed
+        # and recover enough uncovered elements
+        recovering_dict = defaultdict()
+        for set_i in removed_from_solution:
+
+            # Checking set size
+            set_length = len(set_collection[set_i])
+            if set_length <= maximum_set_length_allowed:
+
+                # Initialize set value with 0
+                recovering_dict[set_i] = 0
+
+                # Compute how many current-uncovered-elements this set recovers and save it as value in recovering_dict
+                for element in amount_elements_covered_dict:
+                    if amount_elements_covered_dict[element] != 0:
+                        continue
+                    if element in set_collection[set_i]:
+                        recovering_dict[set_i] += 1
+
+                # Divide the set length through amount of recovered elements
+                if recovering_dict[set_i] == 0:
+                    del recovering_dict[set_i]
+                else:
+                    recovering_dict[set_i] = set_length / recovering_dict[set_i]
+
+        # Select a set where the "cost"-value of the recovering_dict is minimum
+        key_of_cost_min = min(recovering_dict.keys(), key=(lambda k: recovering_dict[k]))
+        current_solution_list_indices.append(key_of_cost_min)
+        removed_from_solution.remove(key_of_cost_min)
+        print("\nSet moving from not-in-solution to in-solution: ", key_of_cost_min,
+              "Amount of sets in current solution: ", len(current_solution_list_indices))
+        for element in set_collection[key_of_cost_min]:
+            amount_elements_covered_dict[element] += 1
+
+    # 6. Remove all duplicates
+    solution_indices = set(current_solution_list_indices)
+
 
     return solution_indices
 
@@ -99,3 +175,8 @@ if __name__ == "__main__":
     wds_2 = {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'
     }
+
+    bla = random.sample(sets_universe, 1)
+    print(bla)
+    print(bla.__class__)
+    print(sets_universe.__class__)
